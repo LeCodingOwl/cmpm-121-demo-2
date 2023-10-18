6dc5c4cdb08c42f07dba484fe49966697afbde8f
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-10-18 00:58:22 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,40 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d")!;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+//const paths = [];
+
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+        ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+    cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+const canvasStartX = 0;
+const canvasStartY = 0;
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(canvasStartX, canvasStartY, canvas.width, canvas.height);
+});
