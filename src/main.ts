0e2c78f3e9a7c4c35c98c3b07caedf4f60ba49c0
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-10-18 00:30:43 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Jimmy's game";
+const gameName = "Sketchy App";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
