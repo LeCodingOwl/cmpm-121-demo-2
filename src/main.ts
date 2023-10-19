da786ea397ae6542fea764dab9bdc7762708fcb7
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-10-18 21:49:44 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -17,37 +17,60 @@ app.append(canvas);
 
 const ctx = canvas.getContext("2d")!;
 
+//const cursor = { active: false, x: 0, y: 0 };
+const lines: { x: number; y: number }[][] = [];
+const redoLines: { x: number; y: number }[][] = [];
+let currentLine: { x: number; y: number }[] | null = null;
+
+const drawingChanged = new Event("drawing-changed", {});
+
+const start = 0;
+const offset = 1;
+
 const cursor = { active: false, x: 0, y: 0 };
 
-//const paths = [];
-
 canvas.addEventListener("mousedown", (e) => {
-    cursor.active = true;
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push(currentLine);
+  redoLines.splice(start, redoLines.length);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+
+  canvas.dispatchEvent(drawingChanged);
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    if (cursor.active) {
-        ctx.beginPath();
-        ctx.moveTo(cursor.x, cursor.y);
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-        cursor.x = e.offsetX;
-        cursor.y = e.offsetY;
-    }
+  if (cursor.active) {
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+    currentLine!.push({ x: cursor.x, y: cursor.y });
+    canvas.dispatchEvent(drawingChanged);
+  }
 });
 
 canvas.addEventListener("mouseup", () => {
-    cursor.active = false;
+  cursor.active = false;
+
+  currentLine = null;
+  canvas.dispatchEvent(drawingChanged);
 });
 
-const clearButton = document.createElement("button");
-clearButton.innerHTML = "clear";
-app.append(clearButton);
+canvas.addEventListener("drawing-changed", () => {
+  ctx.clearRect(start, start, canvas.width, canvas.height);
 
-const canvasStartX = 0;
-const canvasStartY = 0;
-clearButton.addEventListener("click", () => {
-    ctx.clearRect(canvasStartX, canvasStartY, canvas.width, canvas.height);
+  for (const line of lines) {
+    if (line.length > offset) {
+      ctx.beginPath();
+      const { x, y } = line[start];
+      ctx.moveTo(x, y);
+
+      for (const { x, y } of line) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
 });
