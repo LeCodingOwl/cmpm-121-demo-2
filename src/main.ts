e936a625bf4b7a1b1a84523cdefffbf5843a1b18
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-10-18 22:46:22 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ app.append(canvas);
 
 const ctx = canvas.getContext("2d")!;
 
-//const cursor = { active: false, x: 0, y: 0 };
 const lines: { x: number; y: number }[][] = [];
 const redoLines: { x: number; y: number }[][] = [];
 let currentLine: { x: number; y: number }[] | null = null;
@@ -59,6 +58,10 @@ canvas.addEventListener("mouseup", () => {
 });
 
 canvas.addEventListener("drawing-changed", () => {
+  redraw();
+});
+
+function redraw() {
   ctx.clearRect(start, start, canvas.width, canvas.height);
 
   for (const line of lines) {
@@ -72,5 +75,38 @@ canvas.addEventListener("drawing-changed", () => {
       }
       ctx.stroke();
     }
+  }
+}
+
+app.append(document.createElement("br"));
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  lines.splice(0, lines.length);
+  canvas.dispatchEvent(drawingChanged);
+});
+
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "undo";
+app.append(undoButton);
+
+undoButton.addEventListener("click", () => {
+  if (lines.length > 0) {
+    redoLines.push(lines.pop()!);
+    canvas.dispatchEvent(drawingChanged);
+  }
+});
+
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "redo";
+app.append(redoButton);
+
+redoButton.addEventListener("click", () => {
+  if (redoLines.length > 0) {
+    lines.push(redoLines.pop()!);
+    canvas.dispatchEvent(drawingChanged);
   }
 });
